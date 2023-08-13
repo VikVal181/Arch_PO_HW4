b88f215b752c617d4c44109c5d990fbc4069fba2
Vika <vvalujkina@gmail.com>
2023-08-13 16:00:25 +0300

    hw4

---
+++ src/Models/Carrier.java
@@ -0,0 +1,30 @@
+package Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Модель перевозчика
+ */
+public class Carrier {
+    private int id;
+    private long cardNumber;
+    private List<Integer> zones;
+
+
+    public Carrier(int id, long cardNumber) {
+        this.id = id;
+        this.cardNumber = cardNumber;
+        zones = new ArrayList<Integer>();
+        zones.add(1);
+        zones.add(2);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
+}
